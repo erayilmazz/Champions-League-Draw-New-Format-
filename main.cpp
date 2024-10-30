d6e44fce96b5a7e969264ee1c3cc1c662405cb44
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-30 22:52:20 +0300

    create_pots_and_teams created.

--- main.cpp
+++ main.cpp
@@ -3,4 +3,47 @@
 #include <string>
 #include <vector>
 
+void create_pots();
 
+int main(){
+    create_pots_and_teams();
+}
+
+void create_pots_and_teams(){
+    std::string name;
+    std::string country;
+    Pot pot1;
+    for (size_t i = 1; i < 8; i++){
+        std::cout << "Enter Pot 1's " << i << ". team name: ";
+        std::cin >> name;
+        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cin >> country;
+        Team* newTeam = new Team(name, country);
+        pot1.addTeam(newTeam);
+    }
+    Pot pot2;
+    for (size_t i = 1; i < 8; i++){
+        std::cout << "Enter Pot 2's " << i << ". team name: ";
+        std::cin >> name;
+        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cin >> country;
+        Team* newTeam = new Team(name, country);
+        pot2.addTeam(newTeam);
+    }
+    for (size_t i = 1; i < 8; i++){
+        std::cout << "Enter Pot 3's " << i << ". team name: ";
+        std::cin >> name;
+        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cin >> country;
+        Team* newTeam = new Team(name, country);
+        pot3.addTeam(newTeam);
+    }
+    for (size_t i = 1; i < 8; i++){
+        std::cout << "Enter Pot 1's " << i << ". team name: ";
+        std::cin >> name;
+        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cin >> country;
+        Team* newTeam = new Team(name, country);
+        pot4.addTeam(newTeam);
+    }
+}
