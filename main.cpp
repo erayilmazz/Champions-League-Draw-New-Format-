abe18de9b145bafb52791c8e079d63fc6aad7bc8
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-30 23:07:34 +0300

    create pots and teams added and it is worked.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "../head/pot.hpp"
+#include "head/pot.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 
-void create_pots();
+void create_pots_and_teams();
 
 int main(){
     create_pots_and_teams();
@@ -13,35 +13,37 @@ void create_pots_and_teams(){
     std::string name;
     std::string country;
     Pot pot1;
-    for (size_t i = 1; i < 8; i++){
+    for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 1's " << i << ". team name: ";
         std::cin >> name;
-        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cout << "Enter " << name <<"'s country: ";
         std::cin >> country;
         Team* newTeam = new Team(name, country);
         pot1.addTeam(newTeam);
     }
     Pot pot2;
-    for (size_t i = 1; i < 8; i++){
+    for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 2's " << i << ". team name: ";
         std::cin >> name;
-        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cout << "Enter " << name <<"'s country: ";
         std::cin >> country;
         Team* newTeam = new Team(name, country);
         pot2.addTeam(newTeam);
     }
-    for (size_t i = 1; i < 8; i++){
+    Pot pot3;
+    for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 3's " << i << ". team name: ";
         std::cin >> name;
-        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cout << "Enter " << name <<"'s country: ";
         std::cin >> country;
         Team* newTeam = new Team(name, country);
         pot3.addTeam(newTeam);
     }
-    for (size_t i = 1; i < 8; i++){
-        std::cout << "Enter Pot 1's " << i << ". team name: ";
+    Pot pot4;
+    for (size_t i = 1; i <= 8; i++){
+        std::cout << "Enter Pot 4's " << i << ". team name: ";
         std::cin >> name;
-        std::cout >> "Enter " >> name >>"'s country: ";
+        std::cout << "Enter " << name <<"'s country: ";
         std::cin >> country;
         Team* newTeam = new Team(name, country);
         pot4.addTeam(newTeam);
