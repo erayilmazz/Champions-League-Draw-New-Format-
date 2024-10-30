84e6127ad4cfc153c0f3e13d63765a6f10ccb24e
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-30 20:26:33 +0300

    team.cpp created.

---
+++ src/team.cpp
@@ -0,0 +1,10 @@
+#include "../head/team.hpp"
+
+Team Team(std::string name, std::string country)
+        ::name(name),country(country);
+
+string Team::getName() {return name;}
+string Team::getCountry() {return country;}
+void Team::changeRank(int newRank) {rank = newRank;}
+int Team::getRank() {return rank;}
+void Team::setOppent(Team oppent) {opponents.push_back(oppent);}
