fd2b490980be3e706f8b9d02d52955a483e91a8a
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-30 20:40:30 +0300

    pot header file created.

---
+++ head/pot.hpp
@@ -0,0 +1,12 @@
+#include "team.hpp"
+#include <vector>
+
+class Pot{
+private:
+    std::vector <*Team> teams;
+
+public:
+    void addTeam(Team team);
+    std::vector getTeams();
+    Team getTeam(int num);
+};
