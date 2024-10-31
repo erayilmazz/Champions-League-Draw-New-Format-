53a34b8b8fa98d61c7b26a80380565fac2113938
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-31 12:07:08 +0300

    randomize vector added.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
 
 void create_pots_and_teams();
 
 int main(){
     create_pots_and_teams();
+
+
 }
 
 void create_pots_and_teams(){
@@ -21,6 +25,11 @@ void create_pots_and_teams(){
         Team* newTeam = new Team(name, country);
         pot1.addTeam(newTeam);
     }
+    std::vector<Team*> pot1Teams = pot1.getTeams();
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::shuffle(pot1Teams.begin(), pot1Teams.end(), engine);
+
     Pot pot2;
     for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 2's " << i << ". team name: ";
@@ -30,6 +39,11 @@ void create_pots_and_teams(){
         Team* newTeam = new Team(name, country);
         pot2.addTeam(newTeam);
     }
+    std::vector<Team*> pot2Teams = pot2.getTeams();
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::shuffle(pot2Teams.begin(), pot2Teams.end(), engine);
+
     Pot pot3;
     for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 3's " << i << ". team name: ";
@@ -39,6 +53,11 @@ void create_pots_and_teams(){
         Team* newTeam = new Team(name, country);
         pot3.addTeam(newTeam);
     }
+    std::vector<Team*> pot3Teams = pot3.getTeams();
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::shuffle(pot3Teams.begin(), pot3Teams.end(), engine);
+
     Pot pot4;
     for (size_t i = 1; i <= 8; i++){
         std::cout << "Enter Pot 4's " << i << ". team name: ";
@@ -48,4 +67,10 @@ void create_pots_and_teams(){
         Team* newTeam = new Team(name, country);
         pot4.addTeam(newTeam);
     }
+    std::vector<Team*> pot4Teams = pot4.getTeams();
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::shuffle(pot4Teams.begin(), pot4Teams.end(), engine);
 }
+
+
